45d09935a6376c4953be0e2e5a7ae82ad0e65170
Jake Hebert <jacobshebert@gmail.com>
2020-08-01 20:58:07 -0500

    Updated typeform container styling to fix bug where it was not clickable

--- src/components/Typeform.js
+++ src/components/Typeform.js
@@ -2,7 +2,7 @@ import React from "react";
 import { ReactTypeformEmbed } from "react-typeform-embed";
 
 const Typeform = ({ url }) => (
-  <ReactTypeformEmbed url={url} opacity={0} style={{ zIndex: "-1" }} />
+  <ReactTypeformEmbed url={url} opacity={0} style={{ position: "relative" }} />
 );
 
 export default Typeform;
